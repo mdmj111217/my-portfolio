75c58312648f11bd2a74fe75acfa9f7c58ded833
Mark Darwin Marasigan <zmdmarasigan@globe.com.ph>
2021-09-15 18:39:31 +0800

    Simple my portfolio app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ElementRef, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  name: string;
+}
+
+@Component({
+  selector: 'dialog-content-dialog',
+  templateUrl: 'dialog-content-dialog.html',
+})
+export class DialogContentDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogContentDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+    onClose() {
+      this.dialogRef.close();
+    }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +26,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio';
+  @ViewChild('name', { static: true }) nameElement: ElementRef;
+  @ViewChild('email', { static: true }) emailElement: ElementRef;
+  @ViewChild('message', { static: true }) messageElement: ElementRef;
+  
+  constructor(
+    public dialog: MatDialog,
+    nameElement: ElementRef,
+    emailElement: ElementRef,
+    messageElement: ElementRef) {
+    this.nameElement = nameElement;
+    this.emailElement = emailElement;
+    this.messageElement = messageElement;
+  }
+
+  onSave() {
+    const dialogRef = this.dialog.open(DialogContentDialog, {
+      width: '250px',
+      data: {name: this.nameElement.nativeElement.value}
+    });
+
+    console.log("Name: ", this.nameElement.nativeElement.value);
+    console.log("Email: ", this.emailElement.nativeElement.value);
+    console.log("Message: ", this.messageElement.nativeElement.value);
+
+    this.nameElement.nativeElement.value = "";
+    this.emailElement.nativeElement.value = "";
+    this.messageElement.nativeElement.value = "";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
